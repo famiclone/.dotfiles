0dc4ee910d0fa210f1a854f1aa0994750f97e08a
Denys Tynok <denys@famiclone.dev>
2023-04-13 15:29:42 +0300

    add nvimtree plugin

--- .config/nvim/lua/famiclone/packer.lua
+++ .config/nvim/lua/famiclone/packer.lua
@@ -14,33 +14,34 @@ return require('packer').startup(function(use)
   use 'sainnhe/sonokai'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {                                      -- Optional
-	      'williamboman/mason.nvim',
-	      run = function()
-		pcall(vim.cmd, 'MasonUpdate')
-	      end,
-	    },
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {
+                                -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},     -- Required
-	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
-	  }
-	}
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },  -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' },  -- Required
+    }
+  }
 
   use {
     "rest-nvim/rest.nvim",
@@ -50,5 +51,11 @@ return require('packer').startup(function(use)
   }
 
   use 'nvim-orgmode/orgmode'
+  use { 'akinsho/org-bullets.nvim', config = function()
+    require('org-bullets').setup()
+  end }
 
+  use 'dhruvasagar/vim-table-mode'
+  use 'nvim-tree/nvim-tree.lua'
 end)
+
