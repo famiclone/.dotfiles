b2558ef55ebef94cb90e8f933f07c75617f8c2c8
Denys Tynok <denys@famiclone.dev>
2023-04-13 09:53:01 +0300

    change path to org files

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -1,8 +1,9 @@
 local orgmode = require("orgmode")
 local setup = orgmode.setup
+local path = "$HOME/Library/Mobile Documents/com~apple~CloudDocs/org"
 
 orgmode.setup_ts_grammar()
 setup({
-  org_agenda_files = {'~/my-orgs/**/*'},
-  org_default_notes_file = '~/my-orgs/refile.org',
+  org_agenda_files = {path .. '/**/*'},
+  org_default_notes_file = path .. '/refile.org',
 })
