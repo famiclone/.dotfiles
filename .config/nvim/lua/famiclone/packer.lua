b4b2e56d9aae334ccaf591288efbd61f253a8e71
Denys Tynok <denys@famiclone.dev>
2023-04-19 09:27:16 +0300

    add nvim packer bootstrap

--- .config/nvim/lua/famiclone/packer.lua
+++ .config/nvim/lua/famiclone/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
@@ -57,5 +70,9 @@ return require('packer').startup(function(use)
   use 'dhruvasagar/vim-table-mode'
   use 'nvim-tree/nvim-tree.lua'
   use 'ahmedkhalf/project.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
