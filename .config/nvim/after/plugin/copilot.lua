3640dc7fa5c3b9008421fbce6baca655dad5b9df
Denys Tynok <denys@famiclone.dev>
2023-04-26 09:14:14 +0300

    copilot vim

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,18 +1,17 @@
---vim.g.copilot_filetypes = {
---    ["*"] = false,
---    ["javascript"] = true,
---    ["javacriptreact"] = true,
---    ["typescriptreact"] = true,
---    ["bash"] = true,
---    ["typescript"] = true,
---    ["lua"] = false,
---    ["rust"] = true,
---    ["c"] = true,
---    ["c#"] = true,
---    ["c++"] = true,
---    ["go"] = true,
---    ["python"] = true,
---  }
---
---vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["javacriptreact"] = true,
+    ["typescriptreact"] = true,
+    ["bash"] = true,
+    ["typescript"] = true,
+    ["lua"] = false,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+  }
 
+vim.g.copilot_no_tab_map = true
